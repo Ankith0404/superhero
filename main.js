a88f8b15b558e715353913aad1c239a2ad064699
Ankith0404 <84068632+Ankith0404@users.noreply.github.com>
2021-06-26 13:03:05 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,16 @@
+var canvas = new fabric.Canvas('myCanvas');
+block_width = 30;
+block_height = 30
+block_x = 10;
+block_y = 10;
+object_of_the_player = "";
+block_image = "";
+
+function player_update()
+{
+    fabric.image.fromUrl("player.png",function(Img){
+
+    });
+}
+
+
